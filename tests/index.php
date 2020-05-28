42b3aa2a02df7686cb170f3eea03357f1b66ba91
mstevz <mstevz.dev@gmail.com>
2020-05-28 18:53:39 +0100

    Changed test

--- tests/index.php
+++ tests/index.php
@@ -7,38 +7,63 @@ use \mstevz\Events\EventDispatcher;
 use \mstevz\Events\Providers\ListenerProvider;
 use \mstevz\Events\Interfaces\IEventSubscriber;
 
+class Reporter {
 
-interface ServiceProvider {
+    private $listeners;
+    private $newsDispatcher;
 
-        public function getService(string $service);
-
-        public function getAllServices() : iterable;
-
-}
-
-class Auth {
-
-    private $services;
 
     public function __construct(){
-        $this->services = get_class_methods($this);
+        $this->listeners = new ListenerProvider();
+        $this->newsDispatcher = new EventDispatcher($this->listeners);
+        $this->listeners->registerEvent(new Event('news'));
     }
 
-    public function login(){
-        //handle
+    public function getSubscribed(object $listener, $handler){
+        $this->listeners->addListener($listener, $handler)
+                        ->on('news');
     }
 
-    public function register(){
+    public function write(){
+        echo "<pre>";
+        $this->newsDispatcher->dispatch(new Event('news'));
 
+        var_dump($this->newsDispatcher);
+        echo "</pre>";
+    }
+
+}
+
+class TVStation {
+
+    private $name;
+
+    public function __construct(string $name){
+            $this->name = $name;
+    }
+
+    public function getName() : string {
+        return $this->name;
+    }
+
+    public function getNewsEvent(object $e){
+        echo "ok";
+        var_dump($e);
     }
 }
 
-var_dump(ProviderFactory::fromObjectMethods(new Auth));
 
 
+$john = new Reporter();
+$doe  = new Reporter();
 
+$chan1 = new TVStation('chan1');
+$chan2 = new TVStation('chan2');
 
+$john->getSubscribed($chan1, 'getNewsEvent');
+$doe->getSubscribed($chan2, 'getNewsEvent');
 
+$john->write();
 
 
 
